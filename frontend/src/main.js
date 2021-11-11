1900ad19c253024cb2a999de19eb9e4721e9cd18
ghirsch <ghirsch@itba.edu.ar>
2021-11-10 22:43:40 -0300

    front.chore: setup i18n

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,6 +3,8 @@ import './tailwind.css'
 import App from './App.vue'
 import { routes } from './routes.js'
 import { createRouter, createWebHistory } from 'vue-router'
+import { createI18n } from 'vue-i18n'
+import locales from '@/locales';
 
 const app = createApp(App)
 
@@ -11,5 +13,12 @@ const router = createRouter({
   routes,
 })
 
+const i18n = createI18n({
+  locale: navigator.language,
+  fallbackLocale: 'en',
+  messages: locales
+})
+
 app.use(router)
+app.use(i18n)
 app.mount('#app')
