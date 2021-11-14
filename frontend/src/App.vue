a9aa1ea4fd72a99ea71f2d216cf016abb183b1de
ghirsch <ghirsch@itba.edu.ar>
2021-11-14 09:36:35 -0300

    front.feat: added footer & SEO configuration

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,12 +2,19 @@
   <metainfo>
     <template v-slot:title="{ content }">{{ content ? `${content} | SITE_NAME` : `SITE_NAME` }}</template>
     <template v-slot:description="{ content }">{{ content ? `${content}` : `SITE_DESCRIPTION` }}</template>
+    <template v-slot:script="{ content }">{{ content }}</template>
+    <template v-slot:og(title)="{ og }">{{ og.title }}</template>
+    <template v-slot:og(description)="{ og }">{{ og.description }}</template>
+    <template v-slot:og(image)="{ og }">{{ og.image }}</template>
+    <template v-slot:og(url)="{ og }">{{ og.url }}</template>
+    <template v-slot:twitter(card)="{ twitter }">{{ twitter.card }}</template>
   </metainfo>
   <div class="relative" ref="app">
     <v-navbar ref="navbar" />
     <main>
       <router-view />
     </main>
+    <v-footer />
   </div>
 </template>
 
@@ -19,7 +26,8 @@ import { useResizeObserver } from '@vueuse/core';
 import { NAV_HEIGHT } from '@/js/constants.js';
 
 // Components
-import vNavbar from '@/components/navbar.vue';
+import vNavbar from '@/components/static/navbar.vue';
+import vFooter from '@/components/static/footer.vue';
 
 let counter = ref(0);
 setInterval(() => {
@@ -38,6 +46,9 @@ useResizeObserver(app, (_) => {
 useMeta({
   title: '',
   description: '',
-  htmlAttrs: { amp: true }
+  htmlAttrs: { amp: true },
+  twitter: {
+    card: "summary"
+  }
 });
 </script>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,7 +1,16 @@
 <template>
   <v-hero :full-height="true" class="bg-custom_gradient_1 bg-cover" title="app.name" subtitle="home.hero.subtitle">
     <template #buttons>
-      <v-button format="secondary" href="/create" target="_self" aria="Go to create page" :external="false" :white="true" text="CREATE" class="mr-sm md:mr-md lg:mr-xl" />
+      <v-button
+        format="secondary"
+        href="/create"
+        target="_self"
+        aria="Go to create page"
+        :external="false"
+        :white="true"
+        text="CREATE"
+        class="mr-sm md:mr-md lg:mr-xl"
+      />
       <v-button format="secondary" href="/interact" target="_self" aria="Go to interact page" :external="false" :white="true" text="INTERACT" />
     </template>
     <a href="#explained" class="text-typography_primary text-center mt-xl text-body_xl flex flex-row items-center justify-center">
@@ -21,6 +30,23 @@ import { useMeta } from 'vue-meta';
 
 useMeta({
   title: 'Homepage',
-  description: 'This is the homepage to our project'
+  description: 'This is the homepage to our project',
+  og: {
+    title: "TITLE",
+    description: "DESCRIPTION",
+    image: "IMAGE",
+    url: "URL" 
+  },
+  script: [
+    {
+      type: 'application/ld+json',
+      json: {
+        '@context': 'http://schema.org',
+        "@type": "WebPage",
+        "name": "Homepage",
+        "description": "This is a description"
+      }
+    }
+  ]
 });
 </script>
