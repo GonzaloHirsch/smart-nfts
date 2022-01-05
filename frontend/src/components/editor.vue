c914da650f83a21752490b54ca226b83a985ebe6
ghirsch <ghirsch@itba.edu.ar>
2022-01-04 22:17:37 -0300

    front.feat: added `highlight.js` & editor form components

---
+++ frontend/src/components/editor.vue
@@ -0,0 +1,108 @@
+<template>
+  <form>
+    <h2 class="text-center text-brand_primary">Features</h2>
+    <div class="divide-y divide-typography_secondary">
+      <div class="form--section">
+        <h5 class="form--title">Contract Information <QuestionMarkCircleIcon class="form--title-icon" /></h5>
+        <div class="flex flex-row justify-between">
+          <v-input
+            id="contractName"
+            name="contractName"
+            placeholder="SmartNFT..."
+            label="Contract Name"
+            v-model="contractData.contractName"
+            class="w-6/12 pr-2"
+          />
+          <v-input
+            id="contractSymbol"
+            name="contractSymbol"
+            placeholder="SNFT..."
+            label="Contract Symbol"
+            v-model="contractData.contractSymbol"
+            class="w-6/12 pl-2"
+          />
+        </div>
+      </div>
+      <div class="form--section">
+        <h5 class="form--title">Permissions <QuestionMarkCircleIcon class="form--title-icon" /></h5>
+      </div>
+      <div class="form--section">
+        <h5 class="form--title">Creation <QuestionMarkCircleIcon class="form--title-icon" /></h5>
+        <v-checkbox
+          id="isMintable"
+          name="isMintable"
+          placeholder="Is Mintable?"
+          label="Is Mintable?"
+          v-model="contractData.isMintable"
+          class="w-6/12"
+        />
+        <v-checkbox
+          id="isPausable"
+          name="isPausable"
+          placeholder="Is Pausable?"
+          label="Is Pausable?"
+          v-model="contractData.isPausable"
+          class="w-6/12"
+        />
+        <v-checkbox
+          id="isBurnable"
+          name="isBurnable"
+          placeholder="Is Burnable?"
+          label="Is Burnable?"
+          v-model="contractData.isBurnable"
+          class="w-6/12"
+        />
+      </div>
+      <div class="form--section">
+        <h5 class="form--title">Metadata <QuestionMarkCircleIcon class="form--title-icon" /></h5>
+      </div>
+    </div>
+    <!-- ACTIONS -->
+    <div class="flex flex-row items-center justify-center">
+      <v-button format="primary" aria="Create a new NFT" :external="false" :white="false" text="SAVE" @click="saveContract" />
+      <v-button format="primary" aria="Create a new NFT" :external="false" :white="false" text="DEPLOY" class="mx-sm" @click="test" />
+      <v-button format="primary" aria="Create a new NFT" :external="false" :white="false" text="DOWNLOAD" @click="test" />
+    </div>
+  </form>
+</template>
+
+<script setup>
+import vButton from '@/components/button.vue';
+import vInput from '@/components/editor/input.vue';
+import vCheckbox from '@/components/editor/checkbox.vue';
+import { QuestionMarkCircleIcon } from '@heroicons/vue/solid';
+import { ref } from 'vue';
+import { useApi } from '@/plugins/api';
+
+const props = defineProps({});
+
+const contractData = ref({
+  contractName: undefined,
+  contractSymbol: undefined,
+  isMintable: false,
+  isPausable: false,
+  isBurnable: false
+});
+
+const api = useApi();
+const test = () => {
+    api.healthCheck();
+};
+const saveContract = () => {
+    api.saveContract('1234', contractData.value);
+}
+</script>
+
+<style>
+.form--title {
+  @apply text-brand_primary flex items-center;
+}
+
+.form--title-icon {
+  @apply h-6 w-6 text-brand_tertiary ml-xs;
+}
+
+.form--section {
+  @apply py-sm;
+}
+</style>
---
+++ frontend/src/components/editor/checkbox.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="flex flex-row items-center">
+    <input
+      class="rounded-sm border-2 border-typography_secondary focus:border-brand_primary bg-transparent transition duration-300 cursor-pointer editor--checkbox"
+      type="checkbox"
+      :name="props.name"
+      :id="props.id"
+      :placeholder="props.placeholder"
+      @input="onInputChanged"
+      :value="modelValue"
+    />
+    <label class="ml-1" :for="props.id">{{ props.label }}</label>
+  </div>
+</template>
+
+<script setup>
+const emit = defineEmits(['update:modelValue']);
+
+const props = defineProps({
+  name: {
+    type: String,
+    required: true
+  },
+  id: {
+    type: String,
+    required: true
+  },
+  placeholder: {
+    type: String,
+    required: true
+  },
+  label: {
+    type: String,
+    required: true
+  },
+  modelValue: {
+    type: Boolean
+  }
+});
+
+const onInputChanged = (e) => {
+  emit('update:modelValue', e.target.checked);
+};
+</script>
+
+<style scoped>
+.editor--checkbox {
+    @apply outline-none shadow-transparent ring-0 !important;
+}
+</style>
