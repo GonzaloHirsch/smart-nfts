2d73a4424e91e95725d6dadcf8412ffaf621478d
ghirsch <ghirsch@itba.edu.ar>
2022-02-11 16:30:04 -0300

    back.fix: corrected error names

--- backend/src/exceptions/noNetwork.exception.ts
+++ backend/src/exceptions/noNetwork.exception.ts
@@ -3,7 +3,7 @@ import { EXCEPTION_NAMES } from "../constants/errors.constants";
 class NoNetworkException extends Error {
     constructor (config: string | undefined) {
         super(`No supported network selected, current selection is ${config}`);
-        this.name = EXCEPTION_NAMES.CONTRACT_NOT_DEPLOYED;
+        this.name = EXCEPTION_NAMES.NO_NETWORK;
     }
 }
 
