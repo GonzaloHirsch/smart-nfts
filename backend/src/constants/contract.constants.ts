2a484f6dfa4d68da3c460ca63b134f31aa7576bf
FLORENCIA PETRIKOVICH <fpetrikovich@itba.edu.ar>
2022-02-17 18:04:35 -0300

    back.feat: contract generation and front customization for unique storage complete. missing hashing logic

--- backend/src/constants/contract.constants.ts
+++ backend/src/constants/contract.constants.ts
@@ -6,6 +6,7 @@ import { AutoIncrementIds } from "../contracts/AutoIncrementIds.contract";
 import { Mintable } from "../contracts/Mintable.contract";
 import { URIStorage } from "../contracts/URIStorage.contract";
 import { Enumerable } from "../contracts/Enumerable.contract";
+import { UniqueStorage } from "../contracts/UniqueStorage.contract";
 
 export const SOLIDITY_VERSION = '0.8.2';
 export const CONTRACT_LICENSE = 'MIT';
@@ -52,6 +53,7 @@ export enum EXTENSIONS {
     Mintable = 'Mintable',
     AutoIncrementIds = 'AutoIncrementIds',
     Ownable = 'Ownable',
+    UniqueStorage = 'UniqueStorage',
 }
 
 export enum FINAL_EXTENSIONS {
@@ -71,6 +73,7 @@ export const EXTENSION_MAP: Map<string, IContractExtension> = new Map([
     [EXTENSIONS.AutoIncrementIds, AutoIncrementIds],
     [EXTENSIONS.ERC721URIStorage, URIStorage],
     [EXTENSIONS.ERC721Enumerable, Enumerable],
+    [EXTENSIONS.UniqueStorage, UniqueStorage],
 ]);
 
 export const REQUIRE_KEYWORD = 'require';
---
+++ backend/src/contracts/UniqueStorage.contract.ts
@@ -0,0 +1,43 @@
+import { EXTENSIONS, CONTRACT_TYPES, STATE_MUTABILITY, VISIBILITY } from "../constants/contract.constants";
+import { IContractExtension, IContractLibrary, IContractMethod, IContractVariable } from "../interfaces/contract.interface";
+import {staticImplements} from '../helpers/global.helper';
+
+@staticImplements<IContractExtension>()
+export abstract class UniqueStorage {
+    
+    public static getExtensionOZImports(): string[] {
+        return [];
+    }
+    public static getExtensionName(): EXTENSIONS {
+        return EXTENSIONS.UniqueStorage;
+    }
+    public static getExtensionLibs(): IContractLibrary[] {
+        return [];
+    }
+
+    public static getExtensionVariables(): IContractVariable[] {
+        return [{
+            name: '_hashExists',
+            type: 'mapping(string => bool)',
+            visibility: VISIBILITY.PRIVATE
+        }];
+    }
+    public static getExtensionMethods(): IContractMethod[] {
+        return [
+            {
+                name: 'safeMint',
+                params: [{
+                    name: 'hash',
+                    type: CONTRACT_TYPES.STRING_MEMORY
+                }],
+                mandatory: false,
+                content: ['require(!_hashExists[_hash], \'Minting an exisiting hash\');\n','_hashExists[hash] = true;\n'],
+                visibility: VISIBILITY.PUBLIC,
+                options: ''
+            },
+        ];
+    }    
+    public static getParentExtension(): EXTENSIONS | null {
+        return null;
+    }    
+}
