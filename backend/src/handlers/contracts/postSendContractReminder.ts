9d8aed9b632ef2e35fb9d84c5e5489b47d138fa3
ghirsch <ghirsch@itba.edu.ar>
2022-02-19 09:42:02 -0300

    back.feat: corrections & multi-language support for emails

--- backend/src/handlers/contracts/postSendContractReminder.ts
+++ backend/src/handlers/contracts/postSendContractReminder.ts
@@ -6,8 +6,10 @@ import { isEmptyBody, isEmptyPathParams, validContractId } from '../../helpers/v
 import GenericException from '../../exceptions/generic.exception';
 import { setHttpErrorMsg } from '../../helpers/errors.helper';
 import { HTTP_ERRORS } from '../../constants/errors.constants';
+import { ACCEPTED_LANGUAGES_MAP, ACCEPTED_LANGUAGES } from '../../constants/general.constants';
 import { typeValidations } from '../../helpers/validations.helper';
 import { sendReminderEmail } from '../../helpers/email.helper';
+import parser from 'accept-language-parser';
 
 const endpoint = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     if (isEmptyPathParams(event.pathParameters) || !validContractId(event.pathParameters!.contractId)) {
@@ -20,12 +22,38 @@ const endpoint = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyRes
     const requestBody = JSON.parse(event.body!);
     const email = requestBody.email as string;
 
+    // Make sure the contract exists
+    const contract = await (await StoredContractService.getInstance()).getEnforcedContractById(event.pathParameters!.contractId!);
+    if (contract.reminder && contract.reminder.date) {
+        // Initial diff is in millis, get diff in hours
+        let diff = (new Date().getTime() - contract.reminder.date.getTime()) / (1000 * 60 * 60);
+        if (diff < 8) {
+            throw new GenericException(setHttpErrorMsg(HTTP_ERRORS.BAD_REQUEST.GENERAL, `Must wait at least ${(8 - diff).toFixed(1)} hours to send a new reminder`));
+        }
+    }
+    
+    // Detect the user language from the request
+    const languages = parser.parse(event.headers['Accept-Language'])
+    let selectedLanguage: string | undefined = undefined;
+    languages.forEach(lang => {
+        if (ACCEPTED_LANGUAGES_MAP[lang.code] && !selectedLanguage) {
+            selectedLanguage = lang.code;
+        }
+    })
+
     // Validate body input
     if (!email || !typeValidations.email(email)) {
         throw new GenericException(setHttpErrorMsg(HTTP_ERRORS.BAD_REQUEST.PARAMS, 'Missing or invalid email address'));
     }
 
-    await sendReminderEmail(email, event.pathParameters!.contractId!);
+    // Send email
+    await sendReminderEmail(email, event.pathParameters!.contractId!, selectedLanguage!);
+
+    // Update internal status
+    contract.reminder.date = new Date();
+    contract.reminder.language = selectedLanguage ?? ACCEPTED_LANGUAGES.EN;
+    contract.markModified('reminder');
+    await contract.save();
     return {
         statusCode: 204,
         body: JSON.stringify({})
--- backend/src/helpers/email.helper.ts
+++ backend/src/helpers/email.helper.ts
@@ -1,14 +1,18 @@
 import AWS from 'aws-sdk';
-const SES = new AWS.SES();
+const SES = new AWS.SES({
+    accessKeyId: process.env.AWS_EMAIL_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_EMAIL_SECRET_ACCESS_KEY,
+    region: process.env.AWS_EMAIL_REGION
+});
 
-export const sendReminderEmail = async (recipient: string, contractId: string): Promise<boolean> => {
+export const sendReminderEmail = async (recipient: string, contractId: string, language: string = 'en'): Promise<boolean> => {
     const params = {
         Destination: {
             ToAddresses: [recipient]
         },
-        Template: 'SmartNFTsReminderEnglish',
+        Template: language === 'es' ? 'SmartNFTsReminderSpanish' : 'SmartNFTsReminderEnglish',
         TemplateData: `{ "contractid":"${contractId}" }`,
-        Source: 'reminders@smart-nfts.gonzalohirsch.com'
+        Source: `${language === 'es' ? 'Recordatorios' : 'Reminders'}@smart-nfts.gonzalohirsch.com`
     };
 
     return await SES.sendTemplatedEmail(params)
