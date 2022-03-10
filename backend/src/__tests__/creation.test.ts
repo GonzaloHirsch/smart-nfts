c15f2daf77f0b5479f3e914ce6d4173dd508bc2b
FLORENCIA PETRIKOVICH <fpetrikovich@itba.edu.ar>
2022-03-10 09:55:32 -0300

    back.feat: metadata testing complete

--- backend/src/__tests__/creation.test.ts
+++ backend/src/__tests__/creation.test.ts
@@ -1,107 +1,107 @@
-// import { IArguments } from "../interfaces/general.interface";
-// import { EXTENSIONS } from "../constants/contract.constants";
-// import CreationService from "../services/creation.service";
-// import InvalidInputException from "../exceptions/invalidInput.exception";
-// import InvalidContractOptionsException from "../exceptions/invalidContractOptionsException.exception";
-// import { LimitSupply } from "../contracts/LimitSupply.contract";
+import { IArguments } from "../interfaces/general.interface";
+import { EXTENSIONS } from "../constants/contract.constants";
+import CreationService from "../services/creation.service";
+import InvalidInputException from "../exceptions/invalidInput.exception";
+import InvalidContractOptionsException from "../exceptions/invalidContractOptionsException.exception";
+import { LimitSupply } from "../contracts/LimitSupply.contract";
 
-// let service: CreationService;
-// let valid: IArguments;
+let service: CreationService;
+let valid: IArguments;
 
-// beforeAll(async () => {
-//     service = CreationService.getInstance();
-//     valid = {
-//         name: 'TestContract',
-//         symbol: 'TST'
-//     };
-// });
+beforeAll(async () => {
+    service = CreationService.getInstance();
+    valid = {
+        name: 'TestContract',
+        symbol: 'TST'
+    };
+});
 
-// describe('Valid Contract Creation', () => {
+describe('Valid Contract Creation', () => {
 
-//     test('no extensions contract is generated', () => {
-//         const extensions: EXTENSIONS[] = [];
-//         const expectedContract = "// SPDX-License-Identifier: MIT\n// Contract created with Smart NFTs: https://smart-nfts.gonzalohirsch.com/\npragma solidity ^0.8.2;\n\nimport \"@openzeppelin/contracts/token/ERC721/ERC721.sol\";\n\ncontract TestContract is ERC721 \n{\n\t\n\tconstructor() ERC721(\"TestContract\", \"TST\") {}\n\t\n}";
+    test('no extensions contract is generated', () => {
+        const extensions: EXTENSIONS[] = [];
+        const expectedContract = "// SPDX-License-Identifier: MIT\n// Contract created with Smart NFTs: https://smart-nfts.gonzalohirsch.com/\npragma solidity ^0.8.2;\n\nimport \"@openzeppelin/contracts/token/ERC721/ERC721.sol\";\n\ncontract TestContract is ERC721 \n{\n\t\n\tconstructor() ERC721(\"TestContract\", \"TST\") {}\n\t\n}";
         
-//         expect(service.genContract(valid.name, valid.symbol, extensions, {})).toBe(expectedContract);
-//     });
+        expect(service.genContract(valid.name, valid.symbol, extensions, {})).toBe(expectedContract);
+    });
 
-//     test('multiple extensions contract is generated', () => {
-//         const extensions: EXTENSIONS[] = [EXTENSIONS.Mintable, EXTENSIONS.AutoIncrementIds, EXTENSIONS.ERC721Enumerable];
-//         const expectedContract = "// SPDX-License-Identifier: MIT\n// Contract created with Smart NFTs: https://smart-nfts.gonzalohirsch.com/\npragma solidity ^0.8.2;\n\nimport \"@openzeppelin/contracts/access/Ownable.sol\";\nimport \"@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol\";\nimport \"@openzeppelin/contracts/token/ERC721/ERC721.sol\";\nimport \"@openzeppelin/contracts/utils/Counters.sol\";\n\ncontract TestContract is ERC721, ERC721Enumerable, Ownable\n{\n\tusing Counters for Counters.Counter;\n\t\n\tCounters.Counter private _tokenIdCounter;\n\t\n\tconstructor() ERC721(\"TestContract\", \"TST\") {}\n\t\n\tfunction safeMint(address to)\n\t\tpublic\n\t\tonlyOwner\n\t{\n\t\tuint256 tokenId = _tokenIdCounter.current();\n\t\t_tokenIdCounter.increment();\n\t\t_safeMint(to, tokenId);\n\t}\n\t\n\t// The following functions are overrides required by Solidity.\n\t\n\tfunction _beforeTokenTransfer(address from, address to, uint256 tokenId)\n\t\tinternal\n\t\toverride(ERC721, ERC721Enumerable)\n\t{\n\t\tsuper._beforeTokenTransfer(from, to, tokenId);\n\t}\n\t\n\tfunction supportsInterface(bytes4 interfaceId)\n\t\tpublic\n\t\tview\n\t\toverride(ERC721, ERC721Enumerable)\n\t\treturns (bool)\n\t{\n\t\treturn super.supportsInterface(interfaceId);\n\t}\n}";
+    test('multiple extensions contract is generated', () => {
+        const extensions: EXTENSIONS[] = [EXTENSIONS.Mintable, EXTENSIONS.AutoIncrementIds, EXTENSIONS.ERC721Enumerable];
+        const expectedContract = "// SPDX-License-Identifier: MIT\n// Contract created with Smart NFTs: https://smart-nfts.gonzalohirsch.com/\npragma solidity ^0.8.2;\n\nimport \"@openzeppelin/contracts/access/Ownable.sol\";\nimport \"@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol\";\nimport \"@openzeppelin/contracts/token/ERC721/ERC721.sol\";\nimport \"@openzeppelin/contracts/utils/Counters.sol\";\n\ncontract TestContract is ERC721, ERC721Enumerable, Ownable\n{\n\tusing Counters for Counters.Counter;\n\t\n\tCounters.Counter private _tokenIdCounter;\n\t\n\tconstructor() ERC721(\"TestContract\", \"TST\") {}\n\t\n\tfunction safeMint(address to)\n\t\tpublic\n\t\tonlyOwner\n\t{\n\t\tuint256 tokenId = _tokenIdCounter.current();\n\t\t_tokenIdCounter.increment();\n\t\t_safeMint(to, tokenId);\n\t}\n\t\n\t// The following functions are overrides required by Solidity.\n\t\n\tfunction _beforeTokenTransfer(address from, address to, uint256 tokenId)\n\t\tinternal\n\t\toverride(ERC721, ERC721Enumerable)\n\t{\n\t\tsuper._beforeTokenTransfer(from, to, tokenId);\n\t}\n\t\n\tfunction supportsInterface(bytes4 interfaceId)\n\t\tpublic\n\t\tview\n\t\toverride(ERC721, ERC721Enumerable)\n\t\treturns (bool)\n\t{\n\t\treturn super.supportsInterface(interfaceId);\n\t}\n}";
         
-//         expect(service.genContract(valid.name, valid.symbol, extensions, {})).toBe(expectedContract);
-//     });
+        expect(service.genContract(valid.name, valid.symbol, extensions, {})).toBe(expectedContract);
+    });
 
-//     test('user input contract is generated', () => {
-//         const extensions: EXTENSIONS[] = [EXTENSIONS.ERC721Enumerable, EXTENSIONS.LimitSupply];
-//         const expectedContract = "// SPDX-License-Identifier: MIT\n// Contract created with Smart NFTs: https://smart-nfts.gonzalohirsch.com/\npragma solidity ^0.8.2;\n\nimport \"@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol\";\nimport \"@openzeppelin/contracts/token/ERC721/ERC721.sol\";\n\ncontract TestContract is ERC721, ERC721Enumerable\n{\n\t\n\tuint256 private _maxSupply;\n\t\n\tconstructor() ERC721(\"TestContract\", \"TST\") {\n\t\t_maxSupply = 5;\n\t}\n\t\n\t// The following functions are overrides required by Solidity.\n\t\n\tfunction _beforeTokenTransfer(address from, address to, uint256 tokenId)\n\t\tinternal\n\t\toverride(ERC721, ERC721Enumerable)\n\t{\n\t\tsuper._beforeTokenTransfer(from, to, tokenId);\n\t}\n\t\n\tfunction supportsInterface(bytes4 interfaceId)\n\t\tpublic\n\t\tview\n\t\toverride(ERC721, ERC721Enumerable)\n\t\treturns (bool)\n\t{\n\t\treturn super.supportsInterface(interfaceId);\n\t}\n}";
+    test('user input contract is generated', () => {
+        const extensions: EXTENSIONS[] = [EXTENSIONS.ERC721Enumerable, EXTENSIONS.LimitSupply];
+        const expectedContract = "// SPDX-License-Identifier: MIT\n// Contract created with Smart NFTs: https://smart-nfts.gonzalohirsch.com/\npragma solidity ^0.8.2;\n\nimport \"@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol\";\nimport \"@openzeppelin/contracts/token/ERC721/ERC721.sol\";\n\ncontract TestContract is ERC721, ERC721Enumerable\n{\n\t\n\tuint256 private _maxSupply;\n\t\n\tconstructor() ERC721(\"TestContract\", \"TST\") {\n\t\t_maxSupply = 5;\n\t}\n\t\n\t// The following functions are overrides required by Solidity.\n\t\n\tfunction _beforeTokenTransfer(address from, address to, uint256 tokenId)\n\t\tinternal\n\t\toverride(ERC721, ERC721Enumerable)\n\t{\n\t\tsuper._beforeTokenTransfer(from, to, tokenId);\n\t}\n\t\n\tfunction supportsInterface(bytes4 interfaceId)\n\t\tpublic\n\t\tview\n\t\toverride(ERC721, ERC721Enumerable)\n\t\treturns (bool)\n\t{\n\t\treturn super.supportsInterface(interfaceId);\n\t}\n}";
         
-//         expect(service.genContract(valid.name, valid.symbol, extensions, {maxSupply: 5})).toBe(expectedContract);
-//     });
-// });
+        expect(service.genContract(valid.name, valid.symbol, extensions, {maxSupply: 5})).toBe(expectedContract);
+    });
+});
 
-// describe('Invalid Contract Creation', () => {
+describe('Invalid Contract Creation', () => {
 
-//     const nameCases = [['spaces', 'Test Contract'], ['empty', ''], ['invalid character', 'Test$'], ['length exceeded', 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa']];
+    const nameCases = [['spaces', 'Test Contract'], ['empty', ''], ['invalid character', 'Test$'], ['length exceeded', 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa']];
 
-//     test.each(nameCases)(
-//         "invalid name type - %s",
-//         (_methodName, invalidName) => {
+    test.each(nameCases)(
+        "invalid name type - %s",
+        (_methodName, invalidName) => {
             
-//             const extensions: EXTENSIONS[] = [EXTENSIONS.Mintable, EXTENSIONS.AutoIncrementIds, EXTENSIONS.ERC721Enumerable];
+            const extensions: EXTENSIONS[] = [EXTENSIONS.Mintable, EXTENSIONS.AutoIncrementIds, EXTENSIONS.ERC721Enumerable];
 
-//             expect(
-//                 () => {service.genContract(invalidName, valid.symbol, extensions, {})}
-//             ).toThrow(InvalidInputException.Type('name', 'string', invalidName));
-//         }
-//     );
+            expect(
+                () => {service.genContract(invalidName, valid.symbol, extensions, {})}
+            ).toThrow(InvalidInputException.Type('name', 'string', invalidName));
+        }
+    );
 
-//     const symbolCases = [['spaces', 'TS T'], ['empty', ''], ['invalid character', 'TST$'], ['length exceeded', 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa']];
+    const symbolCases = [['spaces', 'TS T'], ['empty', ''], ['invalid character', 'TST$'], ['length exceeded', 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa']];
 
-//     test.each(symbolCases)(
-//         "invalid symbol type - %s",
-//         (_methodName, invalidSymbol) => {
+    test.each(symbolCases)(
+        "invalid symbol type - %s",
+        (_methodName, invalidSymbol) => {
             
-//             const extensions: EXTENSIONS[] = [EXTENSIONS.Mintable, EXTENSIONS.AutoIncrementIds, EXTENSIONS.ERC721Enumerable];
+            const extensions: EXTENSIONS[] = [EXTENSIONS.Mintable, EXTENSIONS.AutoIncrementIds, EXTENSIONS.ERC721Enumerable];
             
-//             expect(
-//                 () => {service.genContract(valid.name, invalidSymbol, extensions, {})}
-//             ).toThrow(InvalidInputException.Type('symbol', 'string', invalidSymbol));
-//         }
-//     );
+            expect(
+                () => {service.genContract(valid.name, invalidSymbol, extensions, {})}
+            ).toThrow(InvalidInputException.Type('symbol', 'string', invalidSymbol));
+        }
+    );
 
-//     const extensionCases = [['storage', [EXTENSIONS.UniqueStorage]], ['enumerable', [EXTENSIONS.LimitSupply]]];
+    const extensionCases = [['storage', [EXTENSIONS.UniqueStorage]], ['enumerable', [EXTENSIONS.LimitSupply]]];
 
-//     test.each(extensionCases)(
-//         "missing parent extension - %s",
-//         (_methodName, extensionList) => {
+    test.each(extensionCases)(
+        "missing parent extension - %s",
+        (_methodName, extensionList) => {
                         
-//             expect(
-//                 () => {service.genContract(valid.name, valid.symbol, extensionList as EXTENSIONS[], {})}
-//             ).toThrow(InvalidContractOptionsException);
-//         }
-//     );
+            expect(
+                () => {service.genContract(valid.name, valid.symbol, extensionList as EXTENSIONS[], {})}
+            ).toThrow(InvalidContractOptionsException);
+        }
+    );
 
-//     test('missing user input - limit supply', () => {
+    test('missing user input - limit supply', () => {
         
-//         const extensions: EXTENSIONS[] = [EXTENSIONS.ERC721Enumerable, EXTENSIONS.LimitSupply];
-//         const missingInput = {};
-//         const neededInput = LimitSupply.getExtensionInputs()[0];
+        const extensions: EXTENSIONS[] = [EXTENSIONS.ERC721Enumerable, EXTENSIONS.LimitSupply];
+        const missingInput = {};
+        const neededInput = LimitSupply.getExtensionInputs()[0];
 
-//         expect(
-//             () => {service.genContract(valid.name, valid.symbol, extensions as EXTENSIONS[], missingInput)}
-//         ).toThrow(InvalidInputException.Missing(neededInput.name, neededInput.type));
-//     });
+        expect(
+            () => {service.genContract(valid.name, valid.symbol, extensions as EXTENSIONS[], missingInput)}
+        ).toThrow(InvalidInputException.Missing(neededInput.name, neededInput.type));
+    });
 
-//     test('invalid user input type - limit supply', () => {
+    test('invalid user input type - limit supply', () => {
         
-//         const extensions: EXTENSIONS[] = [EXTENSIONS.ERC721Enumerable, EXTENSIONS.LimitSupply];
-//         const invalidInput = {maxSupply: 'hi'};
-//         const neededInput = LimitSupply.getExtensionInputs()[0];
+        const extensions: EXTENSIONS[] = [EXTENSIONS.ERC721Enumerable, EXTENSIONS.LimitSupply];
+        const invalidInput = {maxSupply: 'hi'};
+        const neededInput = LimitSupply.getExtensionInputs()[0];
 
-//         expect(
-//             () => {service.genContract(valid.name, valid.symbol, extensions as EXTENSIONS[], invalidInput)}
-//         ).toThrow(InvalidInputException.Type(neededInput.name, neededInput.type, invalidInput.maxSupply));
-//     });
+        expect(
+            () => {service.genContract(valid.name, valid.symbol, extensions as EXTENSIONS[], invalidInput)}
+        ).toThrow(InvalidInputException.Type(neededInput.name, neededInput.type, invalidInput.maxSupply));
+    });
 
-// });
+});
--- backend/src/__tests__/metadata.test.ts
+++ backend/src/__tests__/metadata.test.ts
@@ -1,6 +1,6 @@
 import MintingService from "../services/minting.service";
 import { METADATA_DISPLAY_TYPES, METADATA_TYPES } from "../constants/metadata.constants";
-import { IMetadata } from "../interfaces/metadata.interface";
+import { IMetadata, IStandardMetadata } from "../interfaces/metadata.interface";
 import InvalidInputException from "../exceptions/invalidInput.exception";
 
 let metadataDef: IMetadata;
@@ -35,9 +35,9 @@ beforeAll(async () => {
     }
 });
 
-describe('Check validity of metadata arguments', () => {
+describe('Check validity of metadata default arguments', () => {
 
-    test('missing default arguments - name', () => {
+    test('missing default arguments', () => {
         const args = {description: 'Description', attributes: validAttributes}
         expect(
             () => {service._checkAndMapToStandardMetadata(metadataDef, args, true)}
@@ -51,18 +51,99 @@ describe('Check validity of metadata arguments', () => {
         ).toThrow(InvalidInputException.Missing('description', 'string'));
     });
 
-    test('invalid symbol type - name', () => {
+    test('invalid default arguments type - name', () => {
         const args = {name: 52, description: 'Description', attributes: validAttributes}
         expect(
             () => {service._checkAndMapToStandardMetadata(metadataDef, args, true)}
         ).toThrow(InvalidInputException.Type('name', 'string', args.name));
     });
 
-    test('invalid symbol type - description', () => {
+    test('invalid default arguments type - description', () => {
         const args = {name: 'Name', description: 52, attributes: validAttributes}
         expect(
             () => {service._checkAndMapToStandardMetadata(metadataDef, args, true)}
         ).toThrow(InvalidInputException.Type('description', 'string', args.description));
+    });
+
+});
+
+describe('Check validity of metadata attribute arguments', () => {
+
+    test('incorrect number of attributes', () => {
+        const args = {name: 'Name', description: 'Description', attributes: Object.assign({}, validAttributes)}
+        delete args.attributes.Generation;
+        expect(
+            () => {service._checkAndMapToStandardMetadata(metadataDef, args, true)}
+        ).toThrow(InvalidInputException.Count(metadataDef.attributes.length, Object.keys(args.attributes).length));
+    });
+
+    test('invalid attribute argument', () => {
+        const args = {name: 'Name', description: 'Description', attributes: Object.assign({}, validAttributes)}
+        args.attributes.Agresiveness = 125;
+        expect(
+            () => {service._checkAndMapToStandardMetadata(metadataDef, args, true)}
+        ).toThrow(InvalidInputException.Type('Agresiveness', METADATA_DISPLAY_TYPES.BOOST_PERCENTAGE, args.attributes.Agresiveness));
+    });
+
+    test('mispelled attribute name', () => {
+        const args = {name: 'Name', description: 'Description', attributes: Object.assign({}, validAttributes)}
+        delete args.attributes.Species;
+        args.attributes.Spacies = 'Incorrect';
+        expect(
+            () => {service._checkAndMapToStandardMetadata(metadataDef, args, true)}
+        ).toThrow(InvalidInputException.Missing('Species', METADATA_TYPES.STRING));
+    });
+
+    test('incorrect attribute type - string', () => {
+        const args = {name: 'Name', description: 'Description', attributes: Object.assign({}, validAttributes)}
+        args.attributes.Species = 50;
+        expect(
+            () => {service._checkAndMapToStandardMetadata(metadataDef, args, true)}
+        ).toThrow(InvalidInputException.Type('Species', METADATA_TYPES.STRING, args.attributes.Species));
+    });
+
+    test('incorrect attribute type - number', () => {
+        const args = {name: 'Name', description: 'Description', attributes: Object.assign({}, validAttributes)}
+        args.attributes.Generation = 'string';
+        expect(
+            () => {service._checkAndMapToStandardMetadata(metadataDef, args, true)}
+        ).toThrow(InvalidInputException.Type('Generation', METADATA_DISPLAY_TYPES.PLAIN_NUMBER, args.attributes.Generation));
+    });
+
+    test('incorrect attribute type - hasImage', () => {
+        const args = {name: 'Name', description: 'Description', attributes: Object.assign({}, validAttributes)}
+        expect(
+            () => {service._checkAndMapToStandardMetadata(metadataDef, args, false)}
+        ).toThrow(InvalidInputException.Type('hasImage', metadataDef.hasImage.toString(), false));
+    });
+
+});
+
+describe('Valid metadata generation', () => {
+
+    test('metadata is correctly generated', () => {
+        const args = {name: 'Name', description: 'Description', attributes: Object.assign({}, validAttributes)}
+        const result = service._checkAndMapToStandardMetadata(metadataDef, args, true);
+        expect(result).toStrictEqual({
+            name: args.name,
+            description: args.description,
+            attributes: [
+                {
+                    trait_type: 'Generation',
+                    display_type: METADATA_DISPLAY_TYPES.PLAIN_NUMBER,
+                    value: 348
+                },
+                {
+                    trait_type: 'Agresiveness',
+                    display_type: METADATA_DISPLAY_TYPES.BOOST_PERCENTAGE,
+                    value: 52
+                },
+                {
+                    trait_type: 'Species',
+                    value: 'string'
+                }
+            ]
+        });
     });
 
 });
